7ffcb884ea9131c68874035dba5effb4a96e6ac2
hcarneiro <hcarneiro@weboo.biz>
2017-09-29 15:00:45 +0100

    Interface tweaks

--- js/interface.js
+++ js/interface.js
@@ -1,15 +1,27 @@
 Fliplet().then(function () {
   $(window).on('resize', Fliplet.Widget.autosize);
+  var widgetId = Fliplet.Widget.getDefaultId();
 
-  $('form').submit(function (event) {
-    event.preventDefault();
-
+  function save(notifyComplete){
     Fliplet.Widget.save({
       heading: $('#heading').val()
     }).then(function () {
-      Fliplet.Widget.complete();
+      if (notifyComplete) {
+        Fliplet.Widget.complete();
+      } else {
+        Fliplet.Studio.emit('reload-widget-instance', widgetId);
+      }
     });
+  }
+
+  $('form').submit(function (event) {
+    event.preventDefault();
+    save(true);
   });
+
+  $('#heading').on('keyup change paste', $.debounce(function() {
+    save();
+  }, 500));
 
   // Fired from Fliplet Studio when the external save button is clicked
   Fliplet.Widget.onSaveRequest(function () {
